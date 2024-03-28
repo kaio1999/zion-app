a468417a2f7883e39316ff0cca078923e20a1b70
kaio-santos <kaio.bolgal@gmail.com>
2024-03-28 17:31:23 -0300

    map

--- src/screens/index.tsx
+++ src/screens/index.tsx
@@ -1,4 +1,4 @@
 export { default as OnboardingScreen } from './Onboarding/OnboardingScreen'
 export { default as HomeScreen } from './Home/HomeScreen'
-export { default as Camera } from './Camera'
-export { default as Map } from './Map'
+export { default as Camera } from './Camera/CameraScreen'
+export { default as Map } from './Map/MapScreen'
